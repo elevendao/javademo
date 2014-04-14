52bdee215a7f0a4119fbff926795e0ab240a39cc
elevendao <liushuai2005@gmail.com>
2014-04-14 15:30:13 +0800

    quick sort

--- demo/src/main/java/com/edao/codes/algorithm/sort/QuickSort.java
+++ demo/src/main/java/com/edao/codes/algorithm/sort/QuickSort.java
@@ -19,22 +19,44 @@ public class QuickSort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr = Util.genRandomIntArr(20);
+		int len = 10;
+		int[] arr = Util.genRandomIntArr(len);
 		Util.print(arr);
-		new QuickSort().sort(arr);
+		new QuickSort().sort(arr, 0, len-1);
 		Util.print(arr);
 	}
 	
-	public void sort(int[] arr) {
-		for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] > arr[j]) {
-					int tmp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = tmp;
-				}
+	public void sort(int[] arr, int start, int end) {
+		int i=start;
+		int j=end;
+		while (i<j) {
+			// 右侧扫描，找出第一个比key小的值，交换。
+			while(i<j && arr[i]<=arr[j]) {
+				j--;
 			}
+			if (i<j) {
+				int tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+			// 左侧扫描，找出第一个比key大的值，交换。此时key为arr[j]
+			while (i<j && arr[i]<arr[j]) {
+				i++;
+			}
+			if (i<j) {
+				int tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+		
+		// 递归调用， 把Key前面的完成排序
+		if (i-start > 1) {
+			sort(arr, start, i-1);
+		}
+		// 递归调用，把Key后面的完成排序
+		if (end-i > 1) {
+			sort(arr, i+1, end);
 		}
 	}
 
